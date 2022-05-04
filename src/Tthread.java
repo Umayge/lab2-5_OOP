fd24ac953635dcad4396ef8bdd58a789bd1ded13
yaroslav <yaroslav.khaustovich@yandex.ru>
2022-05-05 01:45:09 +0300

    4 потока

---
+++ src/Tthread.java
@@ -0,0 +1,80 @@
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+
+public class Tthread extends Thread
+{
+    private int type;
+    DefaultTableModel model;
+    JTable tableBase;
+    public Tthread(int i, DefaultTableModel model1, JTable tableBase1 )
+    {
+        type = i;
+        model = model1;
+        tableBase = tableBase1;
+    }
+    public Tthread(int i, DefaultTableModel model1)
+    {
+       type = i;
+       model = model1;
+    }
+    public void run()
+    {
+        if(type == 1)
+        {
+            Object shared =  new Object();
+            synchronized (shared)
+            {
+                    try {
+                        XmlOpen xmlOpen = new XmlOpen("Загрузка данных",model,tableBase);
+                    }
+                catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        if(type == 2)
+        {
+            Object shared =  new Object();
+            synchronized (shared)
+            {
+
+                model.addRow(new String[]{"sfgb","aewrhaer","awhra","wt","waghaq","w3e5u"});
+                shared.notifyAll();
+            }
+        }
+        if(type == 3)
+        {
+            Object shared =  new Object();
+            synchronized (shared)
+            {
+                try {
+                    XmlSave xmlSave = new XmlSave("Сохранение данных",model);
+                }
+                catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        if(type == 4)
+        {
+            Object shared =  new Object();
+            synchronized (shared)
+            {
+                try {
+                    pdfSave qwe = new pdfSave("Сохранение таблици В PDF", model);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                try {
+                    htmlSave  qwe2 = new htmlSave("Сохранение таблици в html", model);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }
+
+    }
+}
